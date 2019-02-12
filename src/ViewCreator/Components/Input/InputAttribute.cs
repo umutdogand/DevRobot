a58c0ebd48469149d7493012828335ae433ca044
umutdogand <umut.dogand@gmail.com>
2019-02-12 14:02:48 +0300

    Input

---
+++ src/ViewCreator/Components/Input/InputAttribute.cs
@@ -0,0 +1,129 @@
+﻿namespace ViewCreator.Components
+{
+    using System;
+    [AttributeUsage(AttributeTargets.Property , AllowMultiple = true, Inherited = true)]
+    public class InputAttribute : HtmlComponent, IInput
+    {
+        public InputAttribute() { }
+        public string Accept
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.AcceptKey); }
+            set { SetFeature(HtmlFeaturesFactory.Accept(value)); }
+        }
+        public string Alt
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.AltKey); }
+            set { SetFeature(HtmlFeaturesFactory.Alt(value)); }
+        }
+        public string Autocomplete
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.AutocompleteKey); }
+            set { SetFeature(HtmlFeaturesFactory.Autocomplete(value)); }
+        }
+        public string Autofocus
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.AutofocusKey); }
+            set { SetFeature(HtmlFeaturesFactory.Autofocus(value)); }
+        }
+        public string Checked
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.CheckedKey); }
+            set { SetFeature(HtmlFeaturesFactory.Checked(value)); }
+        }
+        public string Dirname
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.DirnameKey); }
+            set { SetFeature(HtmlFeaturesFactory.Dirname(value)); }
+        }
+        public string Disabled
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.DisabledKey); }
+            set { SetFeature(HtmlFeaturesFactory.Disabled(value)); }
+        }
+        public string Form
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.FormKey); }
+            set { SetFeature(HtmlFeaturesFactory.Form(value)); }
+        }
+        public string FormAction
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.FormActionKey); }
+            set { SetFeature(HtmlFeaturesFactory.FormAction(value)); }
+        }
+        public string Height
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.HeightKey); }
+            set { SetFeature(HtmlFeaturesFactory.Height(value)); }
+        }
+        public string List
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.ListKey); }
+            set { SetFeature(HtmlFeaturesFactory.List(value)); }
+        }
+        public string Max
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.MaxKey); }
+            set { SetFeature(HtmlFeaturesFactory.Max(value)); }
+        }
+        public string Maxlength
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.MaxlengthKey); }
+            set { SetFeature(HtmlFeaturesFactory.Maxlength(value)); }
+        }
+        public string Min
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.MinKey); }
+            set { SetFeature(HtmlFeaturesFactory.Min(value)); }
+        }
+        public string Multiple
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.MultipleKey); }
+            set { SetFeature(HtmlFeaturesFactory.Multiple(value)); }
+        }
+        public string Name
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.NameKey); }
+            set { SetFeature(HtmlFeaturesFactory.Name(value)); }
+        }
+        public string Pattern
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.PatternKey); }
+            set { SetFeature(HtmlFeaturesFactory.Pattern(value)); }
+        }
+        public string Placeholder
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.PlaceholderKey); }
+            set { SetFeature(HtmlFeaturesFactory.Placeholder(value)); }
+        }
+        public string Readonly
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.ReadonlyKey); }
+            set { SetFeature(HtmlFeaturesFactory.Readonly(value)); }
+        }
+        public string Required
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.RequiredKey); }
+            set { SetFeature(HtmlFeaturesFactory.Required(value)); }
+        }
+        public string Size
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.SizeKey); }
+            set { SetFeature(HtmlFeaturesFactory.Size(value)); }
+        }
+        public string Src
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.SrcKey); }
+            set { SetFeature(HtmlFeaturesFactory.Src(value)); }
+        }
+        public string Width
+        {
+            get { return GetFeature<String>(HtmlFeaturesFactory.WidthKey); }
+            set { SetFeature(HtmlFeaturesFactory.Width(value)); }
+        }
+        public InputType Type
+        {
+            get { return GetFeature<InputType>(HtmlFeaturesFactory.TypeKey); }
+            set { SetFeature(HtmlFeaturesFactory.Type(value)); }
+        }
+    }
+}
---
+++ src/ViewCreator/Components/Input/InputType.cs
@@ -0,0 +1,37 @@
+﻿namespace ViewCreator.Components
+{
+    public class InputType
+    {
+        private string _value;
+
+        public static readonly InputType Button = new InputType("button");
+        public static readonly InputType Checkbox = new InputType("checkbox");
+        public static readonly InputType Color = new InputType("color");
+        public static readonly InputType Date = new InputType("date");
+        public static readonly InputType DatetimeLocal = new InputType("datetime-local");
+        public static readonly InputType Email = new InputType("email");
+        public static readonly InputType File = new InputType("file");
+        public static readonly InputType Hidden = new InputType("hidden");
+        public static readonly InputType Image = new InputType("image");
+        public static readonly InputType Month = new InputType("month");
+        public static readonly InputType Number = new InputType("number");
+        public static readonly InputType Password = new InputType("password");
+        public static readonly InputType Radio = new InputType("radio");
+        public static readonly InputType Range = new InputType("range");
+        public static readonly InputType Reset = new InputType("reset");
+        public static readonly InputType Search = new InputType("search");
+        public static readonly InputType Submit = new InputType("submit");
+        public static readonly InputType Tel = new InputType("tel");
+        public static readonly InputType Text = new InputType("text");
+        public static readonly InputType Time = new InputType("time");
+        public static readonly InputType Url = new InputType("url");
+        public static readonly InputType Week = new InputType("week");
+
+        public string Value => _value;
+
+        public InputType(string value)
+        {
+            this._value = value;
+        }
+    }
+}
